74d24f9e8298e71afb5043cb23d60f6590ada979
Julie Johnson <jjgator417@gmail.com>
2019-08-25 18:56:56 -0500

    messin around with stylesheet app

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import style from './search.module.scss';
 
 const Search: React.FC = () => {
     return (
         <div className="search">
             <form>
                 <label>Search</label>
-                <input>
-
+                <input className={style.red}>
                 </input>
                 <button>Search</button>
             </form>
